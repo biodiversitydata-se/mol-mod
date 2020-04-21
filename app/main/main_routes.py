dd29baea5a4dacca82dae1ff0356bf4a097f4990
Maria Prager <maria.prager@scilifelab.se>
2020-04-21 13:50:19 +0200

    Add comments

--- app/main/main_routes.py
+++ app/main/main_routes.py
@@ -58,9 +58,10 @@ def blast():
     sform = BlastSearchForm()
     rform = BlastResultForm()
 
+    # If Search was clicked, and settings are valid
     if request.form.get('blast_for_seq') and sform.validate_on_submit():
 
-        # Make list of BLAST parameters
+        # Collect BLAST cmd items into list
         cmd = ['blastn']  # [sform.blast_algorithm.data]
         e_val = int(sform.e_value_factor.data) * 10**int(sform.e_value_exponent.data)
         cmd += ["-evalue", str(e_val)]
@@ -73,7 +74,7 @@ def blast():
         # Spawn system process (BLAST) and direct data to file handles
         with subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
                               stderr=subprocess.PIPE) as process:
-            # Send seq from sform to stdin, read output & error until eof
+            # Send seq from sform to stdin, read output & error until 'eof'
             blast_stdout, stderr = process.communicate(input=sform.sequence.data.encode())
             # Get exit status
             returncode = process.returncode
@@ -85,12 +86,12 @@ def blast():
                 # Read into dataframe
                 df = pd.read_csv(stdout_buf, sep='\t', index_col=None, header=None, names=names)
 
-                # If no hits were found
+                # If no hits
                 if len(df) == 0:
                     msg = "No hits were found in the BLAST search"
                     flash(msg, category="error")
 
-                # If hit(s) were found
+                # If some hit(s)
                 else:
                     # Filter not available as blast cmd option...?
                     df = df[df['length'] >= sform.min_aln_length.data]  # Show 1 decimal
@@ -113,14 +114,17 @@ def blast():
             print("BLAST ERROR, output: {}".format(blast_stdout))
             print("BLAST ERROR, stderr: {}".format(stderr))
 
-    # If Show button was clicked, redirect to infraBAS
+    # If Show was clicked (add validation of non-zero selection later)
     if request.form.get('show_occur') and rform.validate_on_submit():
-
+        # Get selected hits from form
         ids = request.form.getlist("asvid")
-        # Add validation of non-zero selection later
+
+        # Make url query string
         idstr = '%22%20OR%20taxon_name%3A%22'.join([str(id) for id in ids])
         url = 'http://molecular.infrabas.se/ala-hub/occurrences/search?q=(taxon_name%3A%22' + \
             idstr + '%22)#tab_recordsView'
+
+        # Redirect to SBDI / bioatlas (GET request)
         return redirect(url)
 
     # If nothing has been submitted (or no hits were found)
