545de5ef41fe543f2a31adadc8d47709e2d76ff9
Maria Prager <maria.prager@scilifelab.se>
2020-05-14 14:07:15 +0200

    Move reusable js to included file

--- app/static/main.js
+++ app/static/main.js
@@ -1,34 +1,31 @@
-// function xtest() {
-//     alert('xtest');
-// }
+function mtest() {
+    alert('då');
+    // alert(value);
+}
+
+// Add checked ids to hidden textarea - for POST to SBDI
+function addCheckedToArea(asvBoxes, sbdiArea) {
+    var ids = [];
+    // Add checked ids to list
+    asvBoxes.map(function () {
+        if($(this).is(":checked")){
+            ids.push(this.id);
+        }
+    });
+    // Remove duplicates
+    ids = ids.filter(function(item, i, ids) {
+        return i == ids.indexOf(item);
+    });
+    // Add ids to textarea
+    sbdiArea.val(ids.join('\n'));
+}
 //
-//
-// // Add checked ids to hidden textarea - for POST to SBDI
-// function addCheckedToArea(obj, table) {
-//     // alert('adding checked');
-//     var ids = [1,2,3];
-//     // Add checked ids to list
-//     $(cbox, tbl).map(function () {
-//         ids.push(this.id);
-//     });
-//     alert(ids);
-//     // // Remove duplicates
-//     // ids = ids.filter(function(item, i, ids) {
-//     //     return i == ids.indexOf(item);
-//     // });
-//     // // Add ids to textarea
-//     // $('#raw_names').val(ids.join('\n'));
-// }
-//
-// // Require at least one selected row
-// function requireSelection(cno) {
-//     if (!$('#raw_names').val()) {
-//         // Highlight checkbox column
-//         $('#result_table tr > td:nth-child('+cno+'), #result_table tr>th:nth-child('+cno+')').attr('bgcolor', '#f2e4e4');
-//         // Show error msg
-//         $('#asv_error').attr('style', 'text-align:right; color:#aa4442');
-//         // Stop submission
-//         return false;
-//     }
-//     return true;
-// }
+// Warn & stop if no selection for SBDI submission / download
+function alertNoSelection(cno) {
+    // Colour checkbox column
+    $('#result_table tr > td:nth-child('+cno+'), #result_table tr>th:nth-child('+cno+')').attr('bgcolor', '#f2e4e4');
+    // Show error msg
+    $('#asv_error').attr('style', 'text-align:right; color:#aa4442');
+    // Stop further action
+    return false;
+}
