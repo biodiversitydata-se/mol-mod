0d1582df0f0a422e345c02be7d842399f262bb37
Maria Prager <maria.prager@scilifelab.se>
2020-05-28 14:50:10 +0200

    Test cont.

--- app/static/main.js
+++ app/static/main.js
@@ -4,7 +4,20 @@ $(document).ready(function() {
     var currPage = $(location).attr('href').split("/").pop();
     switch(currPage) {
         case 'blast':
-        alert('This is blast');
+        // Get input seq length for display
+        $('#sequence_textarea').bind('input', function(){
+            $('#sequence_count').text($(this).val().length);
+        });
+
+        var hlpElem = $('#result_table tr > td:nth-child(6), #result_table tr>th:nth-child(6)');
+        var hlpDiv = $('#selection_error');
+
+        // Convert reasults to jQuery dataTable
+        var dataTbl = makeDataTbl(5, hlpElem, hlpDiv)
+        // Enable access to checkboxes in all rows over all pages
+        var allPages = dataTbl.fnGetNodes();
+        var asvBoxes = $('.asv_id', allPages);
+
         break;
         case 'search_api':
         alert('This is api');
@@ -13,7 +26,41 @@ $(document).ready(function() {
         alert('This neither blast nor api');
         break;
     }
+
+    // When any ASV checkbox is changed
+    asvBoxes.change(function () {
+        // Toggle download selection
+        $(this).closest('tr').toggleClass('selectedRow', this.checked);
+        // Remove no-selection warnings (if any)
+        if (this.checked) {
+            hlpElem.removeClass('visHlpElem');
+            hlpDiv.removeClass('visHlpDiv');
+        }
+    });
+
+    // When header checkbox is changed
+    $('#select_all').change(function () {
+        // Toggle all ASV checkboxes
+        asvBoxes.prop('checked', this.checked);
+        // Toggle download selection
+        asvBoxes.closest('tr').toggleClass('selectedRow', this.checked);
+        // Remove no-selection warnings (if any)
+        hlpElem.removeClass('visHlpElem');
+        hlpDiv.removeClass('visHlpDiv');
+    });
+
+    // At SBDI submit
+    $('#rform').submit(function() {
+        // Copy ASV ids to hidden area
+        addCheckedToArea(asvBoxes, $('#raw_names'));
+        // Warn & stop if no selection
+        if (!$('#raw_names').val()) {
+            return alertNoSelection(hlpElem, hlpDiv);
+        }
+    });
+
 });
+
 // Add checked ids to hidden textarea - for POST to SBDI
 function addCheckedToArea(asvBoxes) {
     var ids = [];
@@ -37,6 +84,7 @@ function alertNoSelection(hlpElem, hlpDiv) {
     hlpElem.addClass('visHlpElem');
     return false;
 }
+
 // Make jQuery dataTable from html table
 // hlpElem/Div needs to be passed here
 function makeDataTbl(CheckBoxIdx, hlpElem, hlpDiv) {
