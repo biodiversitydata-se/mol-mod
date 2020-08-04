2bfbe23df7fdf11e6a9606d712ba1fd3954c231b
Maria Prager <maria.prager@scilifelab.se>
2020-08-04 09:56:31 +0200

    Remove unused templates

--- app/admin/admin_routes.py
+++ app/admin/admin_routes.py
@@ -1,20 +1,2 @@
 #!/usr/bin/env python
 '''Perhaps use later for db-admin'''
-
-from flask import Blueprint, flash, make_response, render_template
-from flask import redirect, url_for
-
-import requests
-
-admin_bp = Blueprint('admin_bp', __name__,
-                     template_folder='templates')
-
-
-@admin_bp.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('main_bp.index'))
-    return render_template('login.html', title='Sign In', form=form)
