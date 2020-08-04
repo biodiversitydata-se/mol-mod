22a43963a04022614ac8920b467cdbd669e77b35
Maria Prager <maria.prager@scilifelab.se>
2020-08-04 10:03:18 +0200

    Remove unused db config

--- config.py
+++ config.py
@@ -14,18 +14,13 @@ def get_env_variable(name):
 class Config:
     # Use hardcoded string if env. variable is missing
     SECRET_KEY = get_env_variable('SECRET_KEY') or 'you-will-never-guess'
-    # SESSION_COOKIE_NAME = get_env_variable('SESSION_COOKIE_NAME')
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(Config):
-    # To eg. auto-reload app when changes are detected
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = get_env_variable("DEV_DATABASE_URI")
 
 
 class ProductionConfig(Config):
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = get_env_variable("PROD_DATABASE_URI")
