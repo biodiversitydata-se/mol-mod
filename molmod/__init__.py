b7a7a093c12171b28a0caa0c628f6c830d91ec29
Maria Prager <maria.prager@scilifelab.se>
2020-08-10 15:21:22 +0200

    Use FLASK_ENV in get_config func to set config

--- molmod/__init__.py
+++ molmod/__init__.py
@@ -5,13 +5,16 @@ import os
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 
+from config import get_config
+
 
 def create_app():
     '''Application factory'''
     # Initialize core app (config path relative to app root)
     # 'app' variable = Flask instance
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object(os.environ['APP_SETTINGS'])
+    # app.config.from_object(get_env_variable('APP_SETTINGS'))
+    app.config.from_object(get_config())
     CSRFProtect(app)
 
     from molmod.main import main_routes
