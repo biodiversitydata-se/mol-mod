f5da75315f1a60811694a0bd4c92a44bf0933fc3
Maria Prager <maria.prager@scilifelab.se>
2020-08-28 16:55:25 +0200

    Add csv download option

--- molmod/static/main.js
+++ molmod/static/main.js
@@ -216,7 +216,7 @@ function makeDataTbl(data, columns) {
         "<'row'<'col-md-12't>>" +
         "<'row'<'col-md-3'B><'col-md-3'i><'col-md-6'p>>",
         buttons: [
-            'copy', 'excel'
+            'copy', 'excel', 'csv'
         ]
     });
 
