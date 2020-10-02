b1fecdefceb4909e6ad7aff986035440e3c5a834
Maria Prager <maria.prager@scilifelab.se>
2020-10-02 08:37:17 +0200

    Comment

--- molmod/static/main.js
+++ molmod/static/main.js
@@ -193,7 +193,7 @@ function makeDataTbl(url, columns) {
             data: function () { return $("#sform").serialize(); } // Includes CSRF-token
         },
         columns : columns,
-        order: [[2, 'desc']],
+        order: [[2, 'desc']], // Required for non-orderable col 0
         select: { style: 'multi', selector: 'td:nth-child(1)' }, // Checkbox selection
         // Layout: l=Show.., f=Search, tr=table, i=Showing.., p=pagination
         dom: "<'row'<'col-md-4'l><'col-md-8'f>>" +
