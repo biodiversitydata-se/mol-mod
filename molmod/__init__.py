5125730cd602697f2bc319839924e7fcb0b91987
Maria Prager <maria.prager@scilifelab.se>
2021-01-15 17:05:41 +0100

    Lint

--- molmod/__init__.py
+++ molmod/__init__.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-
-import os
-
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 
@@ -10,10 +7,7 @@ from .config import get_config
 
 def create_app():
     '''Application factory'''
-    # Initialize core app (config path relative to app root)
-    # 'app' variable = Flask instance
     app = Flask(__name__, instance_relative_config=False)
-    # app.config.from_object(get_env_variable('APP_SETTINGS'))
     app.config.from_object(get_config())
     CSRFProtect(app)
 
