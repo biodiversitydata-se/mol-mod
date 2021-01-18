8df2a3c0587a8ab8590b137c5883d74e37307d43
Maria Prager <maria.prager@scilifelab.se>
2021-01-18 17:49:14 +0100

    Add custom error pages

---
+++ molmod/errors.py
@@ -0,0 +1,31 @@
+from flask import current_app, Markup, render_template, request
+from werkzeug.exceptions import default_exceptions, HTTPException
+
+
+def error_handler(error):
+    msg = "Request resulted in {}".format(error)
+    current_app.logger.warning(msg, exc_info=error)
+
+    if isinstance(error, HTTPException):
+        description = error.get_description(request.environ)
+        code = error.code
+        name = error.name
+    else:
+        description = ("We encountered an error "
+                       "while trying to fulfill your request")
+        code = 500
+        name = 'Internal Server Error'
+
+    templates_to_try = ['error_{}.html'.format(code), 'error_generic.html']
+    return render_template(templates_to_try,
+                           code=code,
+                           name=Markup(name),
+                           description=Markup(description),
+                           error=error)
+
+
+def init_app(app):
+    for exception in default_exceptions:
+        app.register_error_handler(exception, error_handler)
+
+    app.register_error_handler(Exception, error_handler)
