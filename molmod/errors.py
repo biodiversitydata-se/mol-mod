5372a01cfcf386fea13ed4d039b484fea3b751c6
Maria Prager <maria.prager@scilifelab.se>
2021-01-19 09:36:22 +0100

    Add code comments

--- molmod/errors.py
+++ molmod/errors.py
@@ -3,19 +3,26 @@ from werkzeug.exceptions import default_exceptions, HTTPException
 
 
 def error_handler(error):
+    """
+    Handles (almost any) error,
+    and renders specific or generic error page
+    """
     # msg = "Request resulted in {}".format(error)
     # current_app.logger.warning(msg, exc_info=error)
 
+    # For 4XX and 5XX level HTTP errors, save specific info
     if isinstance(error, HTTPException):
-        description = error.get_description(request.environ)
         code = error.code
+        description = error.get_description(request.environ)
         name = error.name
+    # For other Exceptions, use general Server error info
     else:
-        description = ("We encountered an error "
-                       "while trying to fulfill your request")
         code = 500
+        name = error.name
+        description = (f"We encountered an error{name}"
+                       "while trying to fulfill your request")
         name = 'Internal Server Error'
-
+    # Format in specific page, if available (eg. 404), otherwise use generic
     templates_to_try = ['error_{}.html'.format(code), 'error_generic.html']
     return render_template(templates_to_try,
                            code=code,
@@ -25,7 +32,12 @@ def error_handler(error):
 
 
 def init_app(app):
+    """
+    Registers above custom function (error_handler) as error handler
+    for any Werkzeug error class, as well as other exceptions
+    """
+    # Werkzeug, see https://werkzeug.palletsprojects.com/en/1.0.x/exceptions/
     for exception in default_exceptions:
         app.register_error_handler(exception, error_handler)
-
+    # Other exceptions, eg.
     app.register_error_handler(Exception, error_handler)
