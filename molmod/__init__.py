6af07fe16eaebb85c6a8e38ac4b9514ce75cea18
Martin Norling <norlingjr@gmail.com>
2021-01-20 12:58:27 +0100

    Add environment dependent log config files

--- molmod/__init__.py
+++ molmod/__init__.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+
+import os
+import json
+
+from logging.config import dictConfig
+
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 
@@ -8,8 +14,21 @@ from . import errors
 
 def create_app():
     '''Application factory'''
+
+    # Figure out environment to set log config
+    environment = os.getenv('FLASK_ENV')
+    if environment != 'production':
+        environment == 'develop'
+
+    # Load log config, and create the log before the flask app, so that the
+    # flask app picks up the config when it's created.
+    log_config = json.load(open(f'log/log_config_{environment}.json'))
+    dictConfig(log_config)
+
+    # Create flask app
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object(get_config())
+
     CSRFProtect(app)
 
     with app.app_context():
