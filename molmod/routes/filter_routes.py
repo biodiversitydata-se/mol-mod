b1ded94e0f3dd8694eef45017a0c5cafa1f6c569
Maria Prager <maria.prager@scilifelab.se>
2021-01-22 14:20:01 +0100

    Simplify URL construction with loops etc.

--- molmod/routes/filter_routes.py
+++ molmod/routes/filter_routes.py
@@ -21,14 +21,13 @@ def search():
     sform = FilterSearchForm()
     rform = FilterResultForm()
 
-    filters = [f.name for f in sform if f.type == 'SelectMultipleField']
-
     # Reapply any dropdown selections after FILTER submit
+    filters = [f.name for f in sform if f.type == 'SelectMultipleField']
     for f in filters:
         selected = [(x, x) for x in request.form.getlist(f) if x]
         if selected:
             sform[f].choices = selected
-            app.logger.debug(f'Selected {f}: {selected}')
+            app.logger.debug(f'Reapplied selections: {f}: {selected}')
 
     # Only include result form if FILTER button was clicked
     if request.form.get('filter_asvs'):
@@ -38,7 +37,7 @@ def search():
 
 @filter_bp.route('/request_drop_options/<field>', methods=['GET', 'POST'])
 def request_drop_options(field):
-    '''Forwards ajax request for filtered dropdown options to
+    '''Forwards (select2) AJAX request for filtered dropdown options to
     postgREST/postgres function, and returns paginated JSON result'''
     # Make dict of posted filters
     # (e.g. selected kingdom(s), received as 'kingdom[]')
@@ -68,103 +67,27 @@ def request_drop_options(field):
 
 @filter_bp.route('/filter_run', methods=['POST'])
 def filter_run():
-    # Set base URL for api search
+    '''Composes API request for filtered ASV occurrences, based on data
+       received in (DataTable) AJAX request, and returns JSON response'''
+
+    # Set base URL for API search
     url = f"{CONFIG.POSTGREST}/app_search_mixs_tax"
 
-    # Example on reducing lines of code using for loops
-    # search_filters = ['gene', 'sub', 'fw_prim', 'rv_prim', 'kingdom', 'phylum', 'classs', 'oorder', 'family', 'genus',
-    #                  'species']
+    # Append row filters based on POST:ed dropdown selections
+    filters = [f for f in request.form if f != 'csrf_token']
+    selections = {f: ','.join(
+        map(str, request.form.getlist(f))) for f in filters}
+    if selections:
+        url += '?'
+        for filter, value in selections.items():
+            url += f'&{filter}=in.({value})'
+    app.logger.debug(f'URL for API request: {url}')
 
-    # selected_genes_and_primers = {}
-    # for search_filter in search_filters:
-    #    value = request.form.getlist(search_filter)
-    #    if value:
-    #        selected_genes_and_primers[search_filter] = ','.join(map(str, value))
-
-    # if selected_genes_and_primers:
-    #    url += '?'
-    #    for search_filter, value in selected_genes_and_primers.items():
-    #        url += f'&{search_filter}=in.({value})'
-
-    # Get selected genes and/or primers
-    gene_lst = request.form.getlist('gene')
-    sub_lst = request.form.getlist('sub')
-    fw_lst = request.form.getlist('fw_prim')
-    rv_lst = request.form.getlist('rv_prim')
-    kingdom_lst = request.form.getlist('kingdom')
-    phylum_lst = request.form.getlist('phylum')
-    class_lst = request.form.getlist('classs')
-    order_lst = request.form.getlist('oorder')
-    family_lst = request.form.getlist('family')
-    genus_lst = request.form.getlist('genus')
-    species_lst = request.form.getlist('species')
-    # Set logical operator for URL filtering
-    op = '?'
-
-    # Modify URL according to selections
-    # GENE
-    if len(gene_lst) > 0:
-        gene = ','.join(map(str, gene_lst))
-        url += f'?gene=in.({gene})'
-        # Use 'AND' for additional criteria, if any
-        op = '&'
-    # SUBREGION
-    if len(sub_lst) > 0:
-        sub = ','.join(map(str, sub_lst))
-        url += f'{op}sub=in.({sub})'
-        op = '&'
-    # FW PRIMER
-    if len(fw_lst) > 0:
-        fw_prim = ','.join(map(str, fw_lst))
-        url += f'{op}fw_prim=in.({fw_prim})'
-        op = '&'
-    if len(rv_lst) > 0:
-        rv_prim = ','.join(map(str, rv_lst))
-        url += f'{op}rv_prim=in.({rv_prim})'
-        op = '&'
-    # KINGDOM
-    if len(kingdom_lst) > 0:
-        kingdom = ','.join(map(str, kingdom_lst))
-        url += f'{op}kingdom=in.({kingdom})'
-        op = '&'
-    # PHYLUM
-    if len(phylum_lst) > 0:
-        phylum = ','.join(map(str, phylum_lst))
-        url += f'{op}phylum=in.({phylum})'
-        op = '&'
-    # CLASS
-    if len(class_lst) > 0:
-        classs = ','.join(map(str, class_lst))
-        url += f'{op}classs=in.({classs})'
-        op = '&'
-    # ORDER
-    if len(order_lst) > 0:
-        orders = ','.join(map(str, order_lst))
-        url += f'{op}oorder=in.({orders})'
-        op = '&'
-    # FAMILY
-    if len(family_lst) > 0:
-        family = ','.join(map(str, family_lst))
-        url += f'{op}family=in.({family})'
-        op = '&'
-    # GENUS
-    if len(genus_lst) > 0:
-        genus = ','.join(map(str, genus_lst))
-        url += f'{op}genus=in.({genus})'
-        op = '&'
-    # SPECIES
-    if len(species_lst) > 0:
-        species = ','.join(map(str, species_lst))
-        url += f'{op}species=in.({species})'
-
-    # Make api request
+    # Make API request
     try:
         response = requests.get(url)
         response.raise_for_status()
     except (requests.ConnectionError, requests.exceptions.HTTPError):
-        # msg = 'Sorry, search is disabled due to DB connection failure.'
-        # # return "Error: " + str(e)
-        # flash(msg, category='error')
         return None
     else:
         # Convert json to list of dicts
