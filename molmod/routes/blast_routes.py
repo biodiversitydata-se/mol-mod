d28dbe12758ab99bedc67b5087e29f1c8781b4bf
Martin Norling <norlingjr@gmail.com>
2021-01-29 10:55:17 +0100

    Move blast process to separate container

--- molmod/routes/blast_routes.py
+++ molmod/routes/blast_routes.py
@@ -1,13 +1,17 @@
-import io
+"""
+This module contains the routes that are involved in blast processing and
+annotation. The blast-worker containers are called from the /blast_run endpoint
+in this module.
+"""
 import json
-import requests
-import subprocess
 
-import pandas as pd
+import requests
+
 from flask import Blueprint, current_app as app, request
 from flask import render_template
 from flask import jsonify
 
+#pylint: disable=import-error
 from molmod.forms import (BlastResultForm, BlastSearchForm)
 from molmod.config import get_config
 
@@ -34,84 +38,54 @@ def blast():
 
 @blast_bp.route('/blast_run', methods=['POST'])
 def blast_run():
-    '''Composes BLAST cmd from data received in (DataTable) AJAX request,
-       and executes this in subprocess. Adds subject sequences to
-       output, via separate function, and returns JSON Response>
-       (or empty string if error occurs)'''
+    """
+    Sends the blast run request to one of the available blast workers, and then
+    adds subject sequences to the output, via a separate function, and returns a
+    JSON Response (or an empty string if error occurs).
+    """
 
-    # Collect BLAST cmd items into list
-    cmd = ['blastn']  # [sform.blast_algorithm.data]
-    cmd += ['-perc_identity', request.form['min_identity']]
-    cmd += ['-qcov_hsp_perc', request.form['min_qry_cover']]
-    cmd += ['-db', app.config['BLAST_DB']]
-    # cmd += ['-db', 'fake-path-to-no-db']
-    names = ['qacc', 'sacc', 'pident', 'qcovhsp', 'evalue']
-    cmd += ['-outfmt', f'6 {" ".join(names)}']
-    # Only report x best High Scorting Pair(s) per sequence hit
-    # cmd += ['-max_hsps', '1']
-    cmd += ['-num_threads', '4']
+    # convert to json to be able to manipulate the data
 
-    # Spawn system process (BLAST) and direct data to file handles
-    try:
-        with subprocess.Popen(cmd, stdin=subprocess.PIPE,
-                              stdout=subprocess.PIPE,
-                              stderr=subprocess.PIPE) as process:
-            # Send seq from sform to stdin, read output & error until 'eof'
-            blast_stdout, stderr = process.communicate(
-                input=request.form['sequence'].encode()
-            )
-            # Get exit status
-            returncode = process.returncode
-    except (Exception) as e:
-        app.logger.error(f'Subprocess returned: {e}')
-    else:
-        # If BLAST worked (no error)
-        if returncode == 0:
-            app.logger.debug('BLAST success')
+    form = dict(request.form.lists())
+    form['db'] = app.config['BLAST_DB']
+    response = requests.post('http://blast-worker:5000/', json=form)
+    if not response.ok:
+        app.logger.error(response.text)
+        # If error: Return '' instead of None, to avoid logging Werkzeug stack
+        # (visible on next request for some reason).
+        # jQuery will display custom error msg
+        return ''
 
-            # Make in-memory file-like string from blast-output
-            with io.StringIO(blast_stdout.decode()) as stdout_buf:
-                # Read into dataframe
-                df = pd.read_csv(
-                    stdout_buf, sep='\t', index_col=None,
-                    header=None, names=names
-                )
+    # If there are results, format them and add subject sequence before
+    # returning.
 
-                # If no hits
-                if len(df) == 0:
-                    return jsonify({"data": []})
+    results = response.json()
+    results = results['data'] if 'data' in results else results
 
-                # If some hit(s)
-                else:
-                    # Set single decimal for Sci not & float
-                    df['evalue'] = df['evalue'].map('{:.1e}'.format)
-                    df = df.round(1)
-                    # Print taxonomy on new line
-                    df['sacc'] = df['sacc'].str.replace(';', '|')
-                    # Extract asvid from sacc = id + taxonomy
-                    df['asv_id'] = df['sacc'].str.split(
-                        '-', expand=True)[0]
+    # format result fields
+    for result in results:
+        # Set single decimal for Sci not & float
+        result['evalue'] = f'{result["evalue"]:.1e}'
+        # set identity and coverage as single decimal floats
+        result['pident'] = f'{result["pident"]:.1f}'
+        result['qcovhsp'] = f'{result["qcovhsp"]:.1f}'
 
-                    # Get Subject sequence via ID
-                    sdict = get_sseq_from_api(df['asv_id'].tolist())
-                    if sdict:
-                        app.logger.debug(f'{len(sdict)} '
-                                         'unique sequences returned from API')
-                        df['asv_sequence'] = df['asv_id'].map(sdict)
-                        return jsonify({'data': df.to_dict('records')})
-                    app.logger.error('No sequences returned från API')
+        # Print taxonomy on new line
+        result['sacc'] = result['sacc'].replace(';', '|')
+        # Extract asvid from sacc = id + taxonomy
+        result['asv_id'] = result['sacc'].split('-')[0]
 
-            # If BLAST error
-        else:
-            app.logger.error(f'{stderr.decode()}')
+    # Get Subject sequence via ID, and add them to the results
+    asv_ids = [f['asv_id'] for f in results]
+    sdict = get_sseq_from_api(asv_ids)
+    for result in results:
+        if result['asv_id'] in sdict:
+            result['asv_sequence'] = sdict[result['asv_id']]
 
-    # If error: Return '' instead of None, to avoid logging Werkzeug stack
-    # (visible on next request for some reason).
-    # jQuery will display custom error msg
-    return ''
+    return jsonify(data=results)
 
 
-def get_sseq_from_api(asv_ids: list = []) -> dict:
+def get_sseq_from_api(asv_ids: list) -> dict:
     ''' Requests Subject sequences from API,
         as these are not available in regular BLAST response'''
 
@@ -122,8 +96,8 @@ def get_sseq_from_api(asv_ids: list = []) -> dict:
     try:
         response = requests.request("POST", url, headers=headers, data=payload)
         response.raise_for_status()
-    except (requests.exceptions.RequestException, Exception) as e:
-        app.logger.error(f'API request for subject sequences returned: {e}')
+    except requests.exceptions.RequestException as ex:
+        app.logger.error('API request for subject sequences returned: %s', ex)
     else:
         sdict = {item['asv_id']: item['asv_sequence']
                  for item in json.loads(response.text)}
