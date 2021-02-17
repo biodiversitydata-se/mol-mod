faa46b3fb17278c6bbdb96042b1d14bce5162223
Martin Norling <norlingjr@gmail.com>
2021-02-17 09:17:26 +0100

    Add --skip-existing flag to generate_secrets.py

--- scripts/generate_secrets.py
+++ scripts/generate_secrets.py
@@ -12,11 +12,18 @@ import secrets
 import sys
 
 
-def generate_secret(filename):
+def generate_secret(filename, skip_existing: bool = False):
     """
     Generates single secret value (using `secrets.token_hex()`) and saves it
     as given `filename`. The permissions of the filename will be set to 400.
+
+    If `skip_existing` is set, the function will just return the content of the
+    currently existing file.
     """
+
+    if skip_existing and os.path.isfile(filename):
+        logging.info("secrets file %s already exists, skipping", filename)
+        return open(filename).read()
 
     logging.info("generating secret token")
     secret = secrets.token_hex()
@@ -36,7 +43,7 @@ def generate_secret(filename):
         return secret
 
 
-def write_config(config_file, template_file, **variables):
+def write_config(config_file, template_file, skip_existing=False, **variables):
     """
     Reads the `template_file`, replaces variables formatted like <var> with the
     content in `variables`.
@@ -45,6 +52,11 @@ def write_config(config_file, template_file, **variables):
         "<user>" -> "postgres"
 
     """
+
+    if skip_existing and os.path.isfile(config_file):
+        logging.info("secrets file %s already exists, skipping", config_file)
+        return
+
     # load template
     template = open(template_file).read()
 
@@ -77,6 +89,8 @@ if __name__ == '__main__':
     PARSER.add_argument('-t', '--template',
                         default='config/postgrest.conf.template',
                         help="postgrest config template file")
+    PARSER.add_argument('--skip-existing', action="store_true",
+                        help="Skip existing secrets")
     PARSER.add_argument('-v', '--verbose', action="count", default=0,
                         help="Increase logging verbosity (default: warning).")
     PARSER.add_argument('-q', '--quiet', action="count", default=3,
@@ -87,8 +101,8 @@ if __name__ == '__main__':
     logging.basicConfig(level=(10*(ARGS.quiet - ARGS.verbose)))
     # E.g. -vv -> (30-10*2=10) debug, -q -> (30+10*1=40) error
 
-    PG_PASS = generate_secret('.secret.postgres_pass')
-    PG_ANON = generate_secret('.secret.anon_pass')
+    PG_PASS = generate_secret('.secret.postgres_pass', ARGS.skip_existing)
+    PG_ANON = generate_secret('.secret.anon_pass', ARGS.skip_existing)
 
     # read template file
     for row in open(ARGS.env):
@@ -106,4 +120,5 @@ if __name__ == '__main__':
             'name': os.getenv('POSTGRES_DB', 'db')
             }
 
-    write_config('.secret.postgrest_config', ARGS.template, **VARS)
+    write_config('.secret.postgrest_config', ARGS.template, ARGS.skip_existing,
+                 **VARS)
