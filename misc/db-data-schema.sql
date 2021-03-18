62ad322e3b07fd1d6c642645a56d228acefd533d
Maria Prager <maria.prager@scilifelab.se>
2021-03-18 11:33:15 +0100

    Add default to insertion_time

--- misc/db-data-schema.sql
+++ misc/db-data-schema.sql
@@ -8,7 +8,7 @@ CREATE SCHEMA IF NOT EXISTS public;
 CREATE TABLE IF NOT EXISTS public.dataset (
     pid BIGSERIAL PRIMARY KEY,
     dataset_id character varying UNIQUE,
-    insertion_time timestamp without time zone,
+    insertion_time timestamp without time zone NOT NULL DEFAULT now(),
     published boolean default false,
     in_bioatlas boolean default false,
     provider_email character varying
