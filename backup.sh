18f964e36be1fcbd1b3e9b1c067faf05a0216232
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2021-04-08 10:28:30 +0200

    DIR is "db" now

--- backup.sh
+++ backup.sh
@@ -9,7 +9,7 @@
 # requiring data to be restored with this script, together with a dump
 # produced with 'data' option.
 
-DIR='misc'
+DIR='db'
 BASE='db-dump'
 TIMESTAMP="$(date +'%Y-%m-%d_%H%M')"
 CONTAINER='asv-db'
