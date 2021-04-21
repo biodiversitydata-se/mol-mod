4be6c7b4b29794f31fbf14bf83e0218702675630
Maria Prager <maria.prager@scilifelab.se>
2021-04-21 15:53:55 +0200

    Add table with summary stats from materialized view to About page
    Ordinary view was painfully slow

--- molmod/routes/main_routes.py
+++ molmod/routes/main_routes.py
@@ -1,4 +1,12 @@
-from flask import Blueprint, abort, render_template
+import json
+
+import requests
+from flask import Blueprint, abort
+from flask import current_app as APP
+from flask import render_template
+from molmod.config import get_config
+
+CONFIG = get_config()
 
 main_bp = Blueprint('main_bp', __name__,
                     template_folder='templates')
@@ -13,4 +21,22 @@ def index():
 @main_bp.route('/about')
 def about():
     # abort(301)
-    return render_template('about.html')
+    stats = get_stats()
+    return render_template('about.html', rows=stats)
+
+
+@main_bp.route('/stats', methods=['GET'])
+def get_stats() -> dict:
+    '''Makes API request for db stats, and returns dict.'''
+
+    url = f"{CONFIG.POSTGREST}/app_about_stats"
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except (requests.ConnectionError, requests.exceptions.HTTPError) as e:
+        APP.logger.error(f'API request for db stats returned: {e}')
+    else:
+        results = json.loads(response.text)
+        # APP.logger.debug(results)
+        return results
