5880735d92c42ac99e2ec1db27d2b7b69e99f892
Maria Prager <maria.prager@scilifelab.se>
2022-04-22 08:50:08 +0200

    Comment on db roles and usage of docker secrets in db initialization
    (cherry picked from commit 54725cfeb4e832fb8479166619241a09a2465dd1)

--- db/db-roles.sql
+++ db/db-roles.sql
@@ -3,8 +3,8 @@
 --
 
 \set anon `echo ${PGRST_DB_ANON_ROLE:-web_anon}`
--- This is loaded directly from docker secrets
--- See https://hub.docker.com/_/postgres under Docker secrets
+-- This is loaded directly from docker secret
+-- See compose file & https://hub.docker.com/_/postgres under Docker secrets
 \set passwd `echo ${POSTGRES_PASSWORD}`
 -- These need to be read from container files
 \set iptpass `cat ${POSTGRES_IPT_PASS_FILE}`
@@ -14,11 +14,13 @@
 -- Roles
 --
 
--- PostgREST
+-- Role that PostgREST uses for connecting to the database
 CREATE ROLE authenticator;
 ALTER ROLE authenticator WITH NOSUPERUSER NOINHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD :'passwd';
+-- Role that PostgREST switches into when running queries
 CREATE ROLE :anon;
 ALTER ROLE :anon WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB NOLOGIN NOREPLICATION NOBYPASSRLS PASSWORD :'anonpass';
+-- Role for connecting to database from IPT
 CREATE ROLE ipt;
 ALTER ROLE ipt WITH NOSUPERUSER NOINHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD :'iptpass';
 
@@ -26,15 +28,22 @@ ALTER ROLE ipt WITH NOSUPERUSER NOINHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLIC
 -- Role memberships
 --
 
+-- Make it possible for authenticator to switch into anon role, by making auth. member of anon
 GRANT :anon TO authenticator;
 
-GRANT USAGE ON SCHEMA api TO :anon;
-GRANT USAGE ON SCHEMA api TO ipt;
+--
+-- Privileges on database objects
+--
 
+-- Let web users read all views in dedicated API schema
+GRANT USAGE ON SCHEMA api TO :anon;
 GRANT SELECT ON ALL TABLES IN SCHEMA api TO :anon;
+-- Also allow access to functions needed for paginated select2 dropdowns
+-- & for getting sequences for BLAST hits
 GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA api TO :anon;
 GRANT ALL ON ALL TABLES IN SCHEMA api TO authenticator;
-
+-- Only allow IPT user to access Darwin Core views
+GRANT USAGE ON SCHEMA api TO ipt;
 GRANT SELECT ON TABLE api.dwc_oc_occurrence TO ipt;
 GRANT SELECT ON TABLE api.dwc_oc_mixs TO ipt;
 GRANT SELECT ON TABLE api.dwc_oc_emof TO ipt;
