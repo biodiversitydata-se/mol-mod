12eac239a18334d50ff7bbc6a0abbb73e11cd207
Maria Prager <maria.prager@scilifelab.se>
2023-09-15 14:18:51 +0200

    Use auth usr/pwd for PostgREST login, and drop usnused pwd for anon usr
    PostgREST recommends using authenticator user with limited priveleges, which was already created but not actually used in PostgREST config. Pwd for anon user was not used either, so is dropped. Postgres superuser is still used for admin tasks (utility scripts) and pgAdmin login.

--- db/db-roles.sql
+++ db/db-roles.sql
@@ -1,25 +1,26 @@
 --
--- Environment variables
+-- Psql variables
 --
 
-\set anon `echo ${PGRST_DB_ANON_ROLE:-web_anon}`
--- This is loaded directly from docker secret
--- See compose file & https://hub.docker.com/_/postgres under Docker secrets
-\set passwd `echo ${POSTGRES_PASSWORD}`
--- These need to be read from container files
+-- Get role names from .env
+\set auth `echo ${PGRST_DB_AUTH_ROLE:-auth}`
+\set anon `echo ${PGRST_DB_ANON_ROLE:-anon}`
+-- Read pwds from secrets passed to containers via compose file
+\set authpass `cat ${POSTGRES_AUTH_PASS_FILE}`
 \set iptpass `cat ${POSTGRES_IPT_PASS_FILE}`
-\set anonpass `cat ${PGRST_DB_ANON_PASS_FILE}`
+
 
 --
 -- Roles
 --
 
 -- Role that PostgREST uses for connecting to the database
-CREATE ROLE authenticator;
-ALTER ROLE authenticator WITH NOSUPERUSER NOINHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD :'passwd';
+CREATE ROLE :auth;
+ALTER ROLE :auth WITH NOSUPERUSER NOINHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD :'authpass';
 -- Role that PostgREST switches into when running queries
 CREATE ROLE :anon;
-ALTER ROLE :anon WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB NOLOGIN NOREPLICATION NOBYPASSRLS PASSWORD :'anonpass';
+ALTER ROLE :anon WITH NOSUPERUSER NOINHERIT NOCREATEROLE NOCREATEDB NOLOGIN NOREPLICATION NOBYPASSRLS;
+
 -- Role for connecting to database from IPT
 CREATE ROLE ipt;
 ALTER ROLE ipt WITH NOSUPERUSER NOINHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD :'iptpass';
@@ -28,8 +29,8 @@ ALTER ROLE ipt WITH NOSUPERUSER NOINHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLIC
 -- Role memberships
 --
 
--- Make it possible for authenticator to switch into anon role, by making auth. member of anon
-GRANT :anon TO authenticator;
+-- Make it possible for :auth to switch into anon role, by making auth. member of anon
+GRANT :anon TO :auth;
 
 --
 -- Privileges on database objects
@@ -41,7 +42,7 @@ GRANT SELECT ON ALL TABLES IN SCHEMA api TO :anon;
 -- Also allow access to functions needed for paginated select2 dropdowns
 -- & for getting sequences for BLAST hits
 GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA api TO :anon;
-GRANT ALL ON ALL TABLES IN SCHEMA api TO authenticator;
+
 -- Only allow IPT user to access Darwin Core views
 GRANT USAGE ON SCHEMA api TO ipt;
 GRANT SELECT ON TABLE api.dwc_oc_occurrence TO ipt;
